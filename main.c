6c96a887b4303bdd8e0fb6eeaf05e9b8cdfdaf14
João Pedro Abreu de Souza <joaopedroabreudesouza@cryptolab.net>
2018-06-13 14:27:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -19,9 +19,24 @@ typedef struct Ponto
 int main(int argc, char* argv[])
 {
 	FILE* fp;
+	int size;
+	int rank;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	
+	fp=fopen(argv[1],"r");
+	
+	if(rank==0) //root -- master
+	{
+		
+	}
+	else //ordinal user -- slave
+	{
+		int howMuch = howMuchBytes(fp)/2/size;
+		int initialPosition = 
+	}
+	
 	MPI_Finalize();
 	return 0;
 	
